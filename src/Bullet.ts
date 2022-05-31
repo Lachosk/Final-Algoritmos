1bfaa60f46106b257e7f6b468913a3d0fb448982
Andrés NF23 <narvaezandresfelipe23@gmail.com>
2022-05-31 00:18:00 -0500

    Implementacion caracteristicas bullet

--- src/Bullet.ts
+++ src/Bullet.ts
@@ -1,4 +1,15 @@
 import p5 from 'p5';
 export default class Bullet {
+
   private x: number = 0;
+  private y: number = 0;
+  private damage: number =1;
+  private tamX: number = 47;
+  private tamY: number = 20;
+  private vel: number = 5;
+  private bullet: 
+
+  constructor(x:number, y:number, damage:number, tamX:number, tamY:number, vel:number, bullet:){
+
+  }
 }
